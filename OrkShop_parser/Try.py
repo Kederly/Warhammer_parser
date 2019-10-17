d0a4a54c2d2958f3aadfa3e2b4b2f9b6cc380dcf
Kederly <maksnn@inbox.ru>
2019-10-17 14:31:01 +0300

    Упаковал весь парсер в 1 файл

--- OrkShop_parser/Try.py
+++ OrkShop_parser/Try.py
@@ -1,19 +1,28 @@
-#!/usr/bin/env python3.7
 
 import requests
 from bs4 import BeautifulSoup
 import csv
 import os
 
-url_pars = 'https://goodork.ru/categories/adeptus-astartes-space-marines'
-start_url = url_pars + '?page=1'
-base = url_pars + '?page='
-name = 'Space Marines'
-side1 = 'Imperium'
+os.remove('OrkShop.csv')
+
+with open('OrkShop.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(('title', 'price', 'status', 'url'))
 
 def get_html(url):
     r = requests.get(url)
     return r.text
+
+def get_all_links(html):
+    soup = BeautifulSoup(html, 'lxml')
+    tds = soup.find('div', class_='product-categories product-categories-slim').find_all('div', class_='product-categories-header-container')
+    links = []
+    for td in tds:
+        a = td.find('a').get('href')
+        link = a
+        links.append(link)
+    return links
 
 def get_total_pages(html):
     soup = BeautifulSoup(html, 'html5lib')
@@ -21,29 +30,15 @@ def get_total_pages(html):
     if pages is None:
         total_pages = '1'
     else:
-        pages = \
-        soup.find('div', class_='pagenumberer').find_all('a', class_='pagenumberer-item pagenumberer-item-link')[-1].get('href')
+        pages = soup.find('div', class_='pagenumberer').find_all('a', class_='pagenumberer-item pagenumberer-item-link')[-1].get('href')
         total_pages = pages.split('=')[1]
 
     return int(total_pages)
 
-def write_csv(data):
-    with open('OrkShop.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((data['side'],
-                         data['race'],
-                         data['title'],
-                         data['price'],
-                         data['status'],
-                         data['url']))
-
 def get_page_data(html):
-    soup = BeautifulSoup(html, 'html5lib')
-
+    soup = BeautifulSoup(html, 'lxml')
     ads = soup.find('div', class_='row products-view products-view-tile productview-wow').find_all('div', class_='products-view-block')
-
     for ad in ads:
-
         try:
             title = ad.find('div', class_='products-view-name products-view-name-default').find('a').get('title')
         except:
@@ -64,27 +59,37 @@ def get_page_data(html):
             url = ad.find('figure', class_='products-view-pictures').find('a', class_='products-view-picture-link products-view-shadow-hover').get('href')
         except:
             url = ''
-        race = name
-        side = side1
-        data = {'side': side,
-            'race': race,
-            'title': title,
+
+        data = {'title': title,
             'price': price,
             'status': status,
             'url': url}
         write_csv(data)
 
+def write_csv(data):
+    with open('OrkShop.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow((data['title'],
+                         data['price'],
+                         data['status'],
+                         data['url']))
+
 def main():
-    url = start_url
-    base_url = base
+    url = 'https://goodork.ru/categories/warhammer-40000'
 
-    total_pages = get_total_pages(get_html(url))
+    all_links = get_all_links(get_html(url))
 
-    for i in range(1, total_pages+1):
-        url_gen = base_url + str(i)
-        html = get_html(url_gen)
-        get_page_data(html)
+    for link in all_links:
+        html = get_html(link)
+        get_total_pages(html)
+
+        total_pages = get_total_pages(html)
+
+        for i in range(1, total_pages+1):
+            url_gen = link + '?page=' + str(i)
+            html = get_html(url_gen)
+            get_page_data(html)
+
 
 if __name__ == '__main__':
     main()
-
